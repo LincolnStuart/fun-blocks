9e234e3ada1219790cb9def922084fc828585d1a
Lincoln Stuart <lincoln.stuartlg@gmail.com>
2023-06-09 20:04:50 -0300

    chore: token item adjustments (#33)

--- playground/src/main/java/me/lincolnstuart/funblocks/playground/components/TokenItem.kt
+++ playground/src/main/java/me/lincolnstuart/funblocks/playground/components/TokenItem.kt
@@ -3,6 +3,8 @@ package me.lincolnstuart.funblocks.playground.components
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.text.font.FontStyle
 import me.lincolnstuart.funblocks.core.text.Text
+import me.lincolnstuart.funblocks.core.text.utils.TextMode
+import me.lincolnstuart.funblocks.core.text.utils.regular.RegularWeight
 import me.lincolnstuart.funblocks.essentials.list.CustomListItem
 import me.lincolnstuart.funblocks.foundation.ui.token.color.FunBlocksColors
 
@@ -14,7 +16,12 @@ internal fun TokenItem(
     CustomListItem(
         lead = { Text(text = tokenDescription) },
         highlighted = {
-            Text(text = tokenValue, style = FontStyle.Italic, color = FunBlocksColors.PrimaryDark)
+            Text(
+                text = tokenValue,
+                mode = TextMode.Regular(weight = RegularWeight.Bold),
+                style = FontStyle.Italic,
+                color = FunBlocksColors.PrimaryDark
+            )
         }
     )
 }
