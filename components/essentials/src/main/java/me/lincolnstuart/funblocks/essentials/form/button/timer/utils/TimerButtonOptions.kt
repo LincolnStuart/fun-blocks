b829295093dc996984a971244f72f8b617e46639
Lincoln Stuart <lincoln.stuartlg@gmail.com>
2023-07-09 12:47:12 -0300

    feat: timer button (#37)

---
+++ components/essentials/src/main/java/me/lincolnstuart/funblocks/essentials/form/button/timer/utils/TimerButtonOptions.kt
@@ -0,0 +1,17 @@
+package me.lincolnstuart.funblocks.essentials.form.button.timer.utils
+
+import me.lincolnstuart.funblocks.essentials.form.button.utils.ButtonMode
+import me.lincolnstuart.funblocks.essentials.form.button.utils.ButtonType
+
+/**
+ * Data class that customize button options.
+ *
+ * @param mode [ButtonMode] style.
+ * @param type [ButtonType].
+ * @param blockingTimeInSeconds time the button is inoperative.
+ */
+public data class TimerButtonOptions(
+    val mode: ButtonMode = ButtonMode.Primary,
+    val type: ButtonType = ButtonType.Regular,
+    val blockingTimeInSeconds: Int = 30
+)
