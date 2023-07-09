132eee1b670438b9e0534a5ee0996b65097410c7
LincolnStuart <lincoln.stuartlg@gmail.com>
2023-07-09 12:36:45 -0300

    feat: timer button

--- playground/src/main/java/me/lincolnstuart/funblocks/playground/screens/components/form/ButtonScreen.kt
+++ playground/src/main/java/me/lincolnstuart/funblocks/playground/screens/components/form/ButtonScreen.kt
@@ -1,80 +1,40 @@
 package me.lincolnstuart.funblocks.playground.screens.components.form
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import cafe.adriel.voyager.core.screen.Screen
-import compose.icons.TablerIcons
-import compose.icons.tablericons.CircleDashed
-import me.lincolnstuart.funblocks.core.text.Text
-import me.lincolnstuart.funblocks.essentials.form.button.Button
-import me.lincolnstuart.funblocks.essentials.form.button.utils.ButtonMode
-import me.lincolnstuart.funblocks.essentials.form.button.utils.ButtonOptions
-import me.lincolnstuart.funblocks.essentials.form.button.utils.ButtonType
-import me.lincolnstuart.funblocks.essentials.form.radiobutton.RadioButtonGroup
-import me.lincolnstuart.funblocks.essentials.form.switchbutton.SwitchButtonOption
-import me.lincolnstuart.funblocks.essentials.misc.accordion.Accordion
-import me.lincolnstuart.funblocks.playground.components.Sample
+import cafe.adriel.voyager.navigator.LocalNavigator
+import cafe.adriel.voyager.navigator.currentOrThrow
+import me.lincolnstuart.funblocks.core.surface.Surface
+import me.lincolnstuart.funblocks.essentials.list.SimpleListItem
+import me.lincolnstuart.funblocks.foundation.ui.theme.FunBlocksTheme
+import me.lincolnstuart.funblocks.playground.screens.components.form.button.SimpleButtonScreen
+import me.lincolnstuart.funblocks.playground.screens.components.form.button.TimerButtonScreen
 
 class ButtonScreen : Screen {
 
     @Composable
     override fun Content() {
-        var options by remember {
-            mutableStateOf(ButtonOptions(isEnabled = false))
-        }
-        Sample(
-            component = { Button(description = "Button", options = options) {} }
-        ) {
-            Accordion(title = "Mode") {
-                RadioButtonGroup(
-                    options = listOf(
-                        ButtonMode.Primary,
-                        ButtonMode.Secondary,
-                        ButtonMode.Danger,
-                        ButtonMode.Ghost
-                    ),
-                    selectedOption = options.mode,
-                    onSelectOption = { options = options.copy(mode = it) }
-                ) {
-                    Text(text = it.name)
+        FunBlocksTheme {
+            Surface(
+                modifier = Modifier
+                    .fillMaxSize()
+            ) {
+                val navigator = LocalNavigator.currentOrThrow
+                val scrollState = rememberScrollState()
+                Column(Modifier.verticalScroll(scrollState)) {
+                    SimpleListItem(title = "Button") {
+                        navigator.push(SimpleButtonScreen())
+                    }
+                    SimpleListItem(title = "TimerButton") {
+                        navigator.push(TimerButtonScreen())
+                    }
                 }
             }
-            Accordion(title = "Type") {
-                RadioButtonGroup(
-                    options = listOf(
-                        ButtonType.Small,
-                        ButtonType.Regular
-                    ),
-                    selectedOption = options.type,
-                    onSelectOption = { options = options.copy(type = it) }
-                ) {
-                    Text(text = it.name)
-                }
-            }
-            SwitchButtonOption(
-                description = "Enabled",
-                isOn = options.isEnabled,
-                onClick = { options = options.copy(isEnabled = options.isEnabled.not()) }
-            )
-            SwitchButtonOption(
-                description = "Start Icon",
-                isOn = options.startIcon != null,
-                onClick = {
-                    val icon = if (options.startIcon == null) TablerIcons.CircleDashed else null
-                    options = options.copy(startIcon = icon)
-                }
-            )
-            SwitchButtonOption(
-                description = "End Icon",
-                isOn = options.endIcon != null,
-                onClick = {
-                    val icon = if (options.endIcon == null) TablerIcons.CircleDashed else null
-                    options = options.copy(endIcon = icon)
-                }
-            )
         }
     }
 }
---
+++ playground/src/main/java/me/lincolnstuart/funblocks/playground/screens/components/form/button/TimerButtonScreen.kt
@@ -0,0 +1,56 @@
+package me.lincolnstuart.funblocks.playground.screens.components.form.button
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import cafe.adriel.voyager.core.screen.Screen
+import me.lincolnstuart.funblocks.core.text.Text
+import me.lincolnstuart.funblocks.essentials.form.button.timer.TimerButton
+import me.lincolnstuart.funblocks.essentials.form.button.timer.utils.TimerButtonOptions
+import me.lincolnstuart.funblocks.essentials.form.button.utils.ButtonMode
+import me.lincolnstuart.funblocks.essentials.form.button.utils.ButtonType
+import me.lincolnstuart.funblocks.essentials.form.radiobutton.RadioButtonGroup
+import me.lincolnstuart.funblocks.essentials.misc.accordion.Accordion
+import me.lincolnstuart.funblocks.playground.components.Sample
+
+class TimerButtonScreen : Screen {
+
+    @Composable
+    override fun Content() {
+        var options by remember {
+            mutableStateOf(TimerButtonOptions(blockingTimeInSeconds = 5))
+        }
+        Sample(
+            component = { TimerButton(description = "Button", options = options) {} }
+        ) {
+            Accordion(title = "Mode") {
+                RadioButtonGroup(
+                    options = listOf(
+                        ButtonMode.Primary,
+                        ButtonMode.Secondary,
+                        ButtonMode.Danger,
+                        ButtonMode.Ghost
+                    ),
+                    selectedOption = options.mode,
+                    onSelectOption = { options = options.copy(mode = it) }
+                ) {
+                    Text(text = it.name)
+                }
+            }
+            Accordion(title = "Type") {
+                RadioButtonGroup(
+                    options = listOf(
+                        ButtonType.Small,
+                        ButtonType.Regular
+                    ),
+                    selectedOption = options.type,
+                    onSelectOption = { options = options.copy(type = it) }
+                ) {
+                    Text(text = it.name)
+                }
+            }
+        }
+    }
+}
