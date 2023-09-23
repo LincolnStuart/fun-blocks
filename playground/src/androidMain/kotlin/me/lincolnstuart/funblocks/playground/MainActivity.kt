3e5b94655f5df7e439758446b8b23643a8d89f69
LincolnStuart <lincoln.stuartlg@gmail.com>
2023-09-23 11:26:59 -0300

    refactor: playground kmm

--- playground/src/androidMain/kotlin/me/lincolnstuart/funblocks/playground/MainActivity.kt
+++ playground/src/androidMain/kotlin/me/lincolnstuart/funblocks/playground/MainActivity.kt
@@ -3,14 +3,12 @@ package me.lincolnstuart.funblocks.playground
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import cafe.adriel.voyager.navigator.Navigator
-import me.lincolnstuart.funblocks.playground.screens.HomeScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Navigator(screen = HomeScreen())
+            EntryPoint()
         }
     }
 }
